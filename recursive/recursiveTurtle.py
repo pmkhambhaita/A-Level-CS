bb57d9cabba8f313ef9107153a49bb975714fb61
galileo-dev-ops <142727059+galileo-dev-ops@users.noreply.github.com>
2024-02-09 14:20:02 +0000

    -

--- recursive/recursiveTurtle.py
+++ recursive/recursiveTurtle.py
@@ -2,14 +2,14 @@ import turtle
 t = turtle.Turtle()
 window = turtle.Screen()
 
-colours = ['red', 'green']
+colours = ['red', 'green', 'blue', 'yellow', 'purple', 'orange']
 
 t.pendown()
 def square(size):
     if size <= 5:
         return
     else:
-        t.color(colours[(size//10) % 2])
+        t.color(colours[(size//10) % len(colours)])
         for i in range(4):
             t.forward(size)
             t.right(90)
