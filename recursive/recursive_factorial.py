bf37fb7ff300dd72e8b4937a5bc199f6eeb1d28c
galileo-dev-ops <142727059+galileo-dev-ops@users.noreply.github.com>
2024-02-09 11:29:35 +0000

    -

--- recursive/recursive_factorial.py
+++ recursive/recursive_factorial.py
@@ -1,11 +1,32 @@
 import math
 
 def factorialNo(n):
-    if n == 0:       # base case
-        return 1
+    if n == 1:       # base case
+        return n
     else:         # recursive case          
         return n * factorialNo(n - 1)
     
 print(factorialNo(5)) 
 
 print("Expected: " + str(math.factorial(5)))
+
+'''
+
+TRACE
+---------------------
+factorialNo(4)
+else case
+return 4 * factorialNo(3) == 4 * 6 == **24** ⤣ (return to user)
+    ---------------------
+    factorialNo(3)
+    else case
+    return 3 * factorialNo(2) == 3 * 2 == 6 ⤣
+        ---------------------
+        factorialNo(2)
+        else case
+        return 2 * factorialNo(1) == 2 * 1 == 2 ⤣
+            ---------------------
+            factorialNo(1)
+            1 == 1
+            return 1 ⤣
+''' 
