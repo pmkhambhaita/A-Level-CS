48eea1a8072509458fd5f98c10d8d66146191240
galileo-dev-ops <142727059+galileo-dev-ops@users.noreply.github.com>
2024-04-18 09:28:02 +0100

    -

--- js/trafficlight.js
+++ js/trafficlight.js
@@ -1,4 +1,27 @@
-var products = ['Printer', 'Tablet','Router']
-products.sort();
-console.log(products);
-console.log(products.length);
+// var products = ['Printer', 'Tablet','Router']
+// products.sort();
+// console.log(products);
+// console.log(products.length);
+
+var currentLight = 'red';
+
+function changeLight() {
+    var redLight = document.getElementById('red');
+    var yellowLight = document.getElementById('yellow');
+    var greenLight = document.getElementById('green');
+
+    redLight.style.background = 'white';
+    yellowLight.style.background = 'white';
+    greenLight.style.background = 'white';
+
+    if (currentLight === 'red') {
+        yellowLight.style.background = 'yellow';
+        currentLight = 'yellow';
+    } else if (currentLight === 'yellow') {
+        greenLight.style.background = 'green';
+        currentLight = 'green';
+    } else {
+        redLight.style.background = 'red';
+        currentLight = 'red';
+    }
+}
