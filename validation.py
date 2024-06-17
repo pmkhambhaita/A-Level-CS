96f54d1a1749e34a2b48e7612c0e10a1d1d36068
galileo-dev-ops <142727059+galileo-dev-ops@users.noreply.github.com>
2024-06-17 14:07:31 +0100

    -

--- validation.py
+++ validation.py
@@ -1,2 +1,5 @@
+#number = int(input("Enter a number: "))
+#print("Too low" if number < 1 else "Too high" if number > 9 else "Valid")
+
 number = int(input("Enter a number: "))
-print("Too low" if number < 1 else "Too high" if number > 9 else "Valid")
+print('Valid' if 1 <= number <= 9 else 'Invalid')
