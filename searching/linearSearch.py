5e3719b99f5dba687a587ec0657da40e280b8c3f
galileo-dev-ops <pmkhambhaita@outlook.com>
2024-11-25 09:25:38 +0000

    added linear search algorithm

---
+++ searching/linearSearch.py
@@ -0,0 +1,13 @@
+def linear_search(arr, x):
+    for i in range(len(arr)):
+        if arr[i] == x:
+            return i
+    return -1
+
+arr = [3, 1, 4, 6, 8, 2, 5]
+x = 2
+result = linear_search(arr, x)
+if result != -1:
+    print(f"Element found at index: {result}")
+else:
+    print("Element not found")
