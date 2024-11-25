2a586bf3b249ada2c993f94aa13ede4b46977e92
galileo-dev-ops <pmkhambhaita@outlook.com>
2024-11-25 09:53:48 +0000

    -

--- searching/linearSearch.py
+++ searching/linearSearch.py
@@ -1,12 +1,8 @@
-# linear search algorithm
-
-
 def linear_search(arr, x):
     for i in range(len(arr)):
         if arr[i] == x:
             return i
     return -1
-
 
 arr = [3, 1, 4, 6, 8, 2, 5]
 x = 2
@@ -15,4 +11,5 @@ if result != -1:
     print(f"Element found at index: {result}")
 else:
     print("Element not found")
+
 
